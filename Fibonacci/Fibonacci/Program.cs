489dd1ddea5fd4e285c7a2ed048bbfc81f894624
zdzislaw dyrman <zdzislawdyrman1@gmail.com>
2020-12-04 17:09:18 +0000

    .

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+   class Program
+   {
+      public static int Fibonacci(int n)
+      {
+         int a = 0;
+         int b = 1;
+         // In N steps compute Fibonacci sequence iteratively.
+         for (int i = 0; i < n; i++)
+         {
+            int temp = a;
+            a = b;
+            b = temp + b;
+         }
+         return a;
+      }
+      static void Main(string[] args)
+      {
+            Console.WriteLine("Fibonaci Numbers:");
+         for (int i = 0; i < 15; i++)
+         {
+            Console.Write(Fibonacci(i) + ", ");
+         }
+
+         Console.Read();
+      }
+   }
+}
